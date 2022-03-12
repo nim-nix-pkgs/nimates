18c72eb62eea1e97ec0573aa12c62b10bbb57829
hugosenari <hugosenari@gmail.com>
2022-03-12 04:24:54 -0300

    chore: re index nimates

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  description = ''Client library for the Postmates API'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs."nimates-master".url = "path:./master";
+  inputs."nimates-master".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."nimates-master".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@inputs:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib"];
+  in lib.mkProjectOutput {
+    inherit self nixpkgs;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+    refs = builtins.removeAttrs inputs args;
+  };
+}
